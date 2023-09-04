3649a0405a6715b9a013166c68e744e38860efae
nellievropa <124687833+nellievropa@users.noreply.github.com>
2023-09-04 13:15:43 +0000

    Deploying to gh-pages from @ nellievropa/home_work_08@39f2884e5bf870aea13aaf868260ae5152de22ae 🚀

---
+++ 03-feedback.002c62df.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".js-local"),s=document.querySelector(".js-session");e.addEventListener("click",(()=>{localStorage.setItem("local","test local")})),s.addEventListener("click",(()=>{sessionStorage.setItem("session","test session")}));
+//# sourceMappingURL=03-feedback.002c62df.js.map
