97e9cff998772113986044a03a6bc74077599b40
nellievropa <nellievropa23@gmail.com>
2023-09-06 11:53:45 +0300

    + trottle

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,37 +1,100 @@
 
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
-// const player = new Player('handstick', {
-//     id: "#vimeo-player",
-//     width: 640
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+// щоб зберігати час 
+const CURRENT_TIME = 'videoplayer-current-time';
+
+const getCurrentTime = function (currentTime) {
+	const seconds = currentTime.seconds;
+	localStorage.setItem(CURRENT_TIME, JSON.stringify(seconds));
+  };
+
+player.on('play', function() {
+    console.log('played the video!');
+});
+
+
+
+  player.setCurrentTime(30.456).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+
+
+
+// --------------- 1st variant --------------------
+
+// const iframe = document.querySelector('iframe');
+// const player = new Player(iframe);
+
+// const onPlay = function (data) {
+//     localStorage.setItem('videoplayer-current-time', data.seconds);
+// };
+
+// player.on('timeupdate', throttle(onPlay, 1000));
+
+// const currentTime = Number(localStorage.getItem('videoplayer-current-time'));
+
+// player.setCurrentTime(currentTime).then(function (seconds) {
+//     // seconds = the actual time that the player seeked to
+// }).catch(function (error) {
+//     switch (error.name) {
+//         case 'RangeError':
+//             // the time was less than 0 or greater than the video’s duration
+//             break;
+//         default:
+//             // some other error occurred
+//             break;
+//     }
 // });
 
-const player = document.querySelector('#vimeo-player');
-
-
-
-// player.on('play', function() {
-//     console.log('played the video!');
+// player.setColor('#45a247').then(function (color) {
+//     // the color that was set
+// }).catch(function (error) {
+//     // an error occurred setting the color
 // });
 
-document.querySelector('button.video').onclick = () => {
+// --------------- 2nd variant --------------------
+// const CURRENT_TIME_KEY = 'videoplayer-current-time';
 
-	basicLightbox.create(`
-		<video controls data-id="2">
-			<source src="http://clips.vorwaerts-gmbh.de/VfE_html5.mp4" type="video/mp4">
-		</video>
-	`).show()
-
-}
-
-
-
-
-
-
-// player.getVideoTitle().then(function(title) {
-//     console.log('title:', title);
+// const iframe = document.querySelector('iframe');
+// const player = new Player(iframe, {
+//   loop: true,
+//   fullscreen: true,
+//   quality: '1080p',
 // });
+
+// const getCurrentTime = function (currentTime) {
+//   const seconds = currentTime.seconds;
+//   localStorage.setItem(CURRENT_TIME_KEY, JSON.stringify(seconds));
+// };
+
+// player.on('timeupdate', throttle(getCurrentTime, 1000));
+
+// player.setCurrentTime(JSON.parse(localStorage.getItem(CURRENT_TIME_KEY)) || 0);
+
+// player
+//   .setColor('#d8e0ff')
+//   .then(function (color) {
+//     console.log('The new color value: #D8E0FF');
+//   })
+//   .catch(function (error) {
+//     console.log('An error occurred while setting the color');
+//   });
+
 
 
 // player.getCurrentTime().then(function(seconds) {
